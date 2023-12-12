2c6260edfbc4eeda750995e1cd9d96140470e7ed
kassieohern13 <kohern2022@gmail.com>
2023-12-12 15:16:23 -0500

    final good enough

--- PuppyProject/Assets/Scripts/DestroyBone.cs
+++ PuppyProject/Assets/Scripts/DestroyBone.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,20 @@ using UnityEngine;
 public class DestroyBone : MonoBehaviour
 
 {
- 
     public string boneTag = "Bone";
+    public AudioClip onAwakeSound;
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = gameObject.AddComponent<AudioSource>();
+
+        if (onAwakeSound != null)
+        {
+            audioSource.clip = onAwakeSound;
+            audioSource.Play();
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
